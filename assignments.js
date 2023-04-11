26fa7c077eb61167e8a23c9438db3cbd668a090c
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-11 18:16:20 +0300

    gdgd

--- assignments.js
+++ assignments.js
@@ -1,7 +1,19 @@
-let country = 'Romania';
+let country = 'Portugal';
 let continent = 'Europe';
 let population = 10;
-
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'portuguese';
+const country = 'Portugal';
+const continent = 'Europe';
+const isIsland = false;
+isIsland = true;
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -10,6 +10,8 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
+
 let jonas_matilda = 'JM'
 let $function = 27;
 let PI = 3.1415;
@@ -19,3 +21,48 @@ let myCurrentJob = 'Coder';
 
 
 console.log(myCurrentJob);
+*/
+
+
+//    // doar un rand sau un bloc /**/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(typeof year);
+
+
+console.log(typeof null);
+
+*/
+
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;  const = cand nu se mai schimba valoarea variabilelor
+
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmerdtmann';
+console.log(lastName);*/
+
+

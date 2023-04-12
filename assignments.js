c699955386e1344660a76ed23891a99fb4f94814
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-12 10:50:33 +0300

    w

--- assignments.js
+++ assignments.js
@@ -32,3 +32,26 @@ const description1 =
     ' million people speak ' +
     language;
 console.log(description1);*/
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job} !`;
+console.log(jonasNew);
+
+
+console.log(`Just a regular string ...`);
+
+console.log('String with \n\
+multiple \n\
+linest`);
+
+console.log(`String
+multiple
+lines`)
